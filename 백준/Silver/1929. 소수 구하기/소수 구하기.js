630aee9700e37fe675599533e7b83a287fedfd94
murphybread <nargene@gmail.com>
2025-02-04 13:27:53 +0900

    [Silver III] Title: 소수 구하기, Time: 444 ms, Memory: 17328 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,35 +1,27 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
-let input = [];
-let result = [];
+const [m, n] = input;
+const result = [];
 
-readline.on('line', (line) => {
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', ()=>{
-    const [m,n] = input
-    
-    function isPrime(num){
-        if(num === 1){return false}
-        
-        for(let i =2; i <= Math.sqrt(num) ; i++ ){
-            if(num % i === 0){
-                return false
-            }
-        }
-        
-        return true
+function isPrime(num) {
+    if(num === 1) {
+        return false;
     }
-    for(let i = m ; i <= n; i ++){
-        if( isPrime(i)){
-            result.push(i)
-        }
-        
-    }
-
     
-    result.forEach((r) => console.log(r))
-    process.exit();
-});
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+for(let i = m; i <= n; i++) {
+    if(isPrime(i)) {
+        result.push(i);
+    }
+}
+
+console.log(result.join('\n')); // 각 숫자를 줄바꿈으로 구분하여 출력
