7315b7f50cdd265cec07bf47cfc8541400adb7ac
murphybread <nargene@gmail.com>
2025-02-20 18:48:20 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,24 +1,25 @@
-# SKILL_CODE 2진수 변환 , 모든 스킬 추출
-# 
+# DEVELOPERS 테이블에서 Python이나 C# 스킬을 가진 개발자의 정보를 조회하려 합니다.
+
+# 조건에 맞는 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성해 주세요.
 -- 코드를 작성해주세요
-WITH P_C_CODES AS (
-    SELECT CODE
-    FROM SKILLCODES 
-    WHERE NAME = 'Python' OR NAME = 'C#'
+WITH S AS (
+    SELECT
+        SKILLCODES.CODE C
+    FROM
+        SKILLCODES 
+    WHERE
+        NAME = 'Python'
+        OR NAME = 'C#'
 )
-SELECT
-    ID,
-    EMAIL,
-    FIRST_NAME,
-    LAST_NAME
+SELECT DISTINCT
+    DEV.ID,
+    DEV.EMAIL,
+    DEV.FIRST_NAME,
+    DEV.LAST_NAME
 FROM
-    DEVELOPERS 
-WHERE
-    EXISTS(
-        SELECT 1
-        FROM P_C_CODES 
-        WHERE (DEVELOPERS.SKILL_CODE & P_C_CODES.CODE) > 0
-    )
-    
+    DEVELOPERS DEV
+JOIN S SKILL
+    ON DEV.SKILL_CODE & SKILL.C > 0
 ORDER BY
-    ID
+    DEV.ID
+# 결과는 ID를 기준으로 오름차순 정렬해 주세요.
