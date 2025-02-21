210bf8f4f8cb4aa00b30bb10043037c1b191cdb4
murphybread <nargene@gmail.com>
2025-02-22 00:06:24 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,10 +1,17 @@
+// 연결된 노드의 깊이를 찾는 DFS
+
+// 글로벌 visited 한 개로 방문 여부관리
+
+// dfs에서는 현재 방문중인 노드, visited 여부 확인
+
+
 function solution(n, computers) {    
     const visited = Array(n).fill(false)
     let networkCount = 0; 
     
-    function dfs(computerId, visited, computers) {
+    function dfs(computerId) {
         visited[computerId] = true; // 현재 컴퓨터 방문 처리
-        for (let nextComputerId = 0; nextComputerId < computers.length; nextComputerId++) { // 연결된 컴퓨터 찾기
+        for (let nextComputerId = 0; nextComputerId < n; nextComputerId++) { // 연결된 컴퓨터 찾기
             if (computers[computerId][nextComputerId] === 1 && !visited[nextComputerId]) {
                 dfs(nextComputerId, visited, computers); // 재귀 호출
             }
@@ -14,7 +21,7 @@ function solution(n, computers) {
     
     for (let i = 0; i < n; i++) { // 모든 컴퓨터 순회
         if (!visited[i]) { // 아직 방문하지 않은 컴퓨터라면
-            dfs(i, visited, computers); // dfs 호출
+            dfs(i); // dfs 호출
             networkCount++; // 네트워크 개수 증가
         }
     }
