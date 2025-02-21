1c34a10aea43740d793abaae0453c35c0749b05a
murphybread <nargene@gmail.com>
2025-02-21 14:25:48 +0900

    [level 2] Title: 타겟 넘버, Time: 38.94 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,36 +1,17 @@
 function solution(numbers, target) {
     var answer = 0;
-    const maxDepth = numbers.length
-    const operation = [+1 , -1]
-    const arr = []
-    
-    function dfs (value, depth){
-        if (depth === maxDepth ) {
-            arr.push(value)
-            return 
-        }
-        
-        let originalValue = value  // 원본 value 저장
-        
-        // +연산
-        value = originalValue + numbers[depth]*operation[0]
-        dfs(value , depth+1)
-        
-        // - 연산
-        value = originalValue + numbers[depth]*operation[1]
-        dfs(value , depth+1)
-    }
-
-    // 첫 번째 숫자의 + 케이스
-    dfs(numbers[0], 1)
-    // 첫 번째 숫자의 - 케이스
-    dfs(-numbers[0], 1)
-
-    for (let e of arr){
-        if (e === target){
+    const n = numbers.length
+    function backtracking (depth, sum){
+        if (depth > n ) return
+        if (sum === target && depth === n) {
             answer +=1
+
         }
+        
+        
+        backtracking (depth +1, sum + numbers[depth])
+        backtracking (depth +1, sum - numbers[depth])
     }
-    
+    backtracking(0,0)
     return answer;
 }
