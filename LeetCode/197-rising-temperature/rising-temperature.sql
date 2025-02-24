f7e9832811dd4daa3f50df9b6c2e09486ebf39a7
murphybread <nargene@gmail.com>
2025-02-24 18:15:45 +0900

    Time: 1454 ms (8.53%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/197-rising-temperature/rising-temperature.sql
+++ LeetCode/197-rising-temperature/rising-temperature.sql
@@ -2,9 +2,8 @@
 select
     w1.id
 from
-    Weather w1
-join
+    Weather w1,
     Weather w2
-    on w1.recordDate = DATE_ADD(w2.recordDate , interval 1 day)
 where
-    w1.temperature  > w2.temperature 
+    DATEDIFF(w1.recordDate , w2.recordDate ) = 1
+    and w1.temperature  > w2.temperature 
