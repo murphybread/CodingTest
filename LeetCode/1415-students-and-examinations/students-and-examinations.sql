b065a30f906d7b8400f7acc4ef5fbdc190413aed
murphybread <nargene@gmail.com>
2025-02-25 17:44:13 +0900

    Time: 1289 ms (80.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,34 @@
+WITH StudentSubject AS (
+    SELECT 
+        s.student_id, 
+        s.student_name, 
+        sub.subject_name
+    FROM 
+        Students s, 
+        Subjects sub
+),
+ExamCount AS (
+    SELECT 
+        student_id, 
+        subject_name, 
+        COUNT(*) AS exam_count
+    FROM 
+        Examinations
+    GROUP BY 
+        student_id, subject_name
+)
+
+SELECT 
+    ss.student_id, 
+    ss.student_name, 
+    ss.subject_name, 
+    SUM(CASE WHEN ec.exam_count IS NULL THEN 0 ELSE ec.exam_count END)  AS attended_exams
+FROM 
+    StudentSubject ss
+LEFT JOIN 
+    ExamCount ec ON ss.student_id = ec.student_id AND ss.subject_name = ec.subject_name
+GROUP BY
+    ss.student_id, ss.student_name, ss.subject_name
+ORDER BY 
+    ss.student_id, ss.subject_name;
+
