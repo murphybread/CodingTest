cfc93e4d6db19f1e83781bc78565e37c16c80780
murphybread <nargene@gmail.com>
2025-07-07 23:29:04 +0900

    [D2] Title: 숫자 배열 회전, Time: 52 ms, Memory: 38,016 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,76 +1,82 @@
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-# 표준 입력 예제
-'''
-a = int(input())                        정수형 변수 1개 입력 받는 예제
-b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
-d = float(input())                      실수형 변수 1개 입력 받는 예제
-e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
-h = input()                             문자열 변수 1개 입력 받는 예제
-'''
+public class Solution {
 
-# 표준 출력 예제
-'''
-a, b = 6, 3
-c, d, e = 1.0, 2.5, 3.4
-f = "ABC"
-print(a)                                정수형 변수 1개 출력하는 예제
-print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
-print(c, d, e)                          실수형 변수 3개 출력하는 예제
-print(f)                                문자열 1개 출력하는 예제
-'''
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		// System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringBuilder sb =new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		
+		for (int test_case =1 ;  test_case <=T;test_case++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			
+           int[][] a = new int[N][N];
+           
+           
+            
+            for (int i = 0; i < N; i++) {
+            	StringTokenizer  st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    a[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            int first90[][] =rotate(a);
+            int second180[][] =rotate(first90);
+            int third270[][] =rotate(second180);
+            
+            System.out.println("#" + test_case);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(first90[i][j]);
+                }
+                System.out.print(" ");
 
+                for (int j = 0; j < N; j++) {
+                    System.out.print(second180[i][j]);
+                }
+                System.out.print(" ");
 
+                for (int j = 0; j < N; j++) {
+                    System.out.print(third270[i][j]);
+                }
+                // 한 줄이 모두 완성되면 줄바꿈
+                System.out.println();
+            }
+        }		
+		
+		
 
+		
 
-'''
-아래의 구문은 input.txt 를 read only 형식으로 연 후,
-앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-아래 구문을 사용하기 위해서는 import sys가 필요합니다.
-단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
+		
+		
+		
+		
 
+	}
+	
+	public static int[][] rotate(int[][] arr) {
+		
+	    int N = arr.length;
+	    int[][] rotated = new int[N][N]; 
+	    for (int i = 0; i < N; i++) {
+	        for (int j = 0; j < N; j++) {
+	            rotated[i][j] = arr[N - 1 - j][i];
+	        }
+	    }
 
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    N =int(input())
-    
-    board= []
-    for _ in range(N):
-        line =list(map(int,input().split()))
-        board.append(line)
-    
-        
-    
-    def rotate(matrix):
-        
-        new = [ [0]*N for _ in range(N)]
-        
-        for i in range(N):
-            for j in range(N):
-                new[j][N-1-i] = matrix[i][j]
-        
-        return new
-    
-    one =rotate(board)
-    two = rotate(one)
-    three =rotate(two)
-    
-    print(f'#{test_case}')
-    for i in range(N):
-        print(''.join(list(map(str,one[i]))), ''.join(list(map(str,two[i]))),''.join(list(map(str,three[i]))) , end=' ')
-        
-        print('')
-        
-    # ///////////////////////////////////////////////////////////////////////////////////
-    '''
+	    return rotated; // 결과 배열을 반환
+		
+		
+	}
 
-        이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-
-    '''
-    # ///////////////////////////////////////////////////////////////////////////////////
+}
